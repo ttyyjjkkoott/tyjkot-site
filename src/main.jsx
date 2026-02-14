6f6d1af86f3159c80aa84482f72db962d0a778da
ttyyjjkkoott <tyjkot@proton.me>
2026-02-14 14:37:58 -0600

    Fixed import in main.jsx to locate app.jsx properly

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import MinimalBlog from './App.jsx'
+import MinimalBlog from './app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
